e890c188411ba0a354513bbb1ab5493c0024a422
Ezo <j96rqcn87f@privaterelay.appleid.com>
2025-12-03 14:00:50 +0000

    firebase SDK

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -3,12 +3,12 @@
 // Keep this file local and do NOT commit real keys to public repos if you don't want them public.
 
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_WITH_YOUR_PROJECT",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID"
+  apiKey: "AIzaSyAH4xqnFtnRwAzcRCKNWewce2sVf5kmCQg",
+  authDomain: "i-stem-comms.firebaseapp.com",
+  projectId: "i-stem-comms",
+  storageBucket: "i-stem-comms.firebasestorage.app",
+  messagingSenderId: "609185253106",
+  appId: "1:609185253106:web:5d11c0bf82bb556531b26b"
 };
 
 // Example Firestore structure expected:
